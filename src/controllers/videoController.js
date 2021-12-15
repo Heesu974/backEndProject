e91c2c0bb09fe48d1cc2f07cd34f88181710d510
heesu.kang <heesu.kang95@gmail.com>
2021-12-15 22:59:15 +0900

    1215 mixin

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,44 @@
-export const trending = (req, res) => res.render("home");
-
-export const see = (req, res) => res.render("watch");
-
-export const edit = (req, res) => {
-    return res.send(`Edit the video #${req.params.id}`);
+const fakeUser = {
+    username: "heesu",
+    loggedIn: true,
 }
+// "/"ㅇㅔ ㄷㅐ하ㄴ controller가 trending이라서 videoController.js에서 작성한 것임
+
+export const trending = (req, res) => {
+    const videos = [
+        {
+            title: "First video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 1,
+        },
+        {
+            title: "Second video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 1,
+        },
+        {
+            title: "Third video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 1,
+        }
+    ];
+
+    return res.render("home", { pageTitle: "home", fakeUser: fakeUser, videos })
+};
+// trending에는 언젠가 video들이 들어갈 것이다.
+
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch", fakeUser: fakeUser });
+
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" })
 export const search = (req, res) => res.send("Search Videos");
 export const deleteVideo = (req, res) => {
     return res.send(`Delete the video #${req.params.id}`);
