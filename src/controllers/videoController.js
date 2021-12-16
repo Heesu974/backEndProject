42d855e7730c5e636b455a2276035406dabfab7d
heesu.kang <heesu.kang95@gmail.com>
2021-12-16 23:11:17 +0900

    connecting pages by ids

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,41 +2,47 @@ const fakeUser = {
     username: "heesu",
     loggedIn: true,
 }
-// "/"ㅇㅔ ㄷㅐ하ㄴ controller가 trending이라서 videoController.js에서 작성한 것임
+const videos = [
+    {
+        title: "First video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id: 1,
+    },
+    {
+        title: "Second video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id: 2,
+    },
+    {
+        title: "Third video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id: 3,
+    }
+];
 
 export const trending = (req, res) => {
-    const videos = [
-        {
-            title: "First video",
-            rating: 5,
-            comments: 2,
-            createdAt: "2 minutes ago",
-            views: 59,
-            id: 1,
-        },
-        {
-            title: "Second video",
-            rating: 5,
-            comments: 2,
-            createdAt: "2 minutes ago",
-            views: 59,
-            id: 1,
-        },
-        {
-            title: "Third video",
-            rating: 5,
-            comments: 2,
-            createdAt: "2 minutes ago",
-            views: 59,
-            id: 1,
-        }
-    ];
+
 
     return res.render("home", { pageTitle: "home", fakeUser: fakeUser, videos })
 };
 // trending에는 언젠가 video들이 들어갈 것이다.
 
-export const see = (req, res) => res.render("watch", { pageTitle: "Watch", fakeUser: fakeUser });
+export const see = (req, res) => {
+    const { id } = req.params; //선택된 파라미터 :id의 id를 받고
+    const video = videos[id - 1] // id에 해당하는 video를 찾고
+    // 컴퓨터 카운팅은 0부터인데, videos[0].id는 1부터 시작해서
+
+    return res.render("watch", { pageTitle: `Watching ${video.title}`, videos });//Watch라는 템플릿을 render할 것이다.
+}
 
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" })
 export const search = (req, res) => res.send("Search Videos");
