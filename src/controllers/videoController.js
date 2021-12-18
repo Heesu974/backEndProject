6f607b3a59cee53e4c249f69d0b24d003087de07
heesu.kang <heesu.kang95@gmail.com>
2021-12-18 22:48:37 +0900

    connecting pages by IDs

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -16,7 +16,7 @@ const videos = [
         rating: 5,
         comments: 2,
         createdAt: "2 minutes ago",
-        views: 59,
+        views: 50,
         id: 2,
     },
     {
@@ -36,12 +36,12 @@ export const trending = (req, res) => {
 };
 // trending에는 언젠가 video들이 들어갈 것이다.
 
-export const see = (req, res) => {
+export const watch = (req, res) => {
     const { id } = req.params; //선택된 파라미터 :id의 id를 받고
     const video = videos[id - 1] // id에 해당하는 video를 찾고
     // 컴퓨터 카운팅은 0부터인데, videos[0].id는 1부터 시작해서
-
-    return res.render("watch", { pageTitle: `Watching ${video.title}`, videos });//Watch라는 템플릿을 render할 것이다.
+    return res.render("watch", { pageTitle: `Watching ${video.title}`, video });//Watch라는 템플릿을 render할 것이다.
+    //이 코드로 인해 watch 템플릿에 video object가 생겼다.
 }
 
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" })
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { see, edit, upload, deleteVideo } from "../controllers/videoController"
+import { watch, edit, upload, deleteVideo } from "../controllers/videoController"
 
 const VideoRouter = express.Router();
 
 VideoRouter.get("/upload", upload);
-VideoRouter.get("/:id(\\d+)", see);
+VideoRouter.get("/:id(\\d+)", watch);
 VideoRouter.get("/:id(\\d+)/edit", edit);
 VideoRouter.get("/:id(\\d+)/delete", deleteVideo);
 
