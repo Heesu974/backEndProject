b7e5be57660d19031816631349655cd424095985
heesu.kang <66421797+Heesu974@users.noreply.github.com>
2021-12-30 19:14:46 +0900

    Exceptions and validation

--- src/models/video.js
+++ src/models/video.js
@@ -6,14 +6,14 @@ import mongoose from "mongoose";
 const videoSchema = new mongoose.Schema({
     //비디오 형식 작성(실제 데이터 x)
     //데이터 타이틀: 데이터 타입
-    title: String,
+    title: { type: String, required: true },
     // title: {type: String},과 똑값다.
-    description: String,
-    createdAt: Date,
+    description: { type: String, required: true },
+    createdAt: { type: Date, required: true, default: Date.now },
     hashtags: [{ type: String }],
     meta: {
-        views: Number,
-        rating: Number
+        views: { type: Number, required: true, default: 0 },
+        rating: { type: Number, required: true, default: 0 }
     }
 })
 // 이것이 video Schema이고, 데이터의 형식/형태(shape)를 정의해주고 있을 뿐, 데이터 값을 입력하는 것이 아니다.데이터 값을 입력하는 것은 유저의 몫이다. 
